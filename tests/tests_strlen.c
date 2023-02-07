a5b91d8a7a0f4f200afd02f642d74f4ddf723c80
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-02-07 20:24:19 +0100

    test: add edge case memory tests for strlen

--- tests/tests_strlen.c
+++ tests/tests_strlen.c
@@ -29,3 +29,23 @@ Test(my_strlen, empty, .init = init, .timeout = 2)
 {
     cr_assert_eq(my_strlen(""), 0);
 }
+
+Test(my_strlen, memory_trick_1, .init = init, .timeout = 2)
+{
+    int result1 = strlen("Coucou");
+    int result2 = my_strlen("Coucou");
+    int result3 = strlen("Coucou");
+
+    cr_assert_eq(result2, 6);
+    cr_assert_eq(result1, result2);
+    cr_assert_eq(result2, result3);
+}
+
+Test(my_strlen, memory_trick_2, .init = init, .timeout = 2)
+{
+    int result1 = strlen("Phrase longue numero une");
+    int result2 = my_strlen("\n\012\034\056\076\011\a\r%sabcd");
+    int result3 = strlen("un petit crash ?");
+
+    cr_assert_eq(result2, 14);
+}
