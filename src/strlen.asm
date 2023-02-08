5e81e774f857001c4827e64d982d501bac8ff6fb
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-02-08 22:19:34 +0100

    refactor: add prologue and epilogue to strlen function

--- src/strlen.asm
+++ src/strlen.asm
@@ -4,6 +4,7 @@ section .text
     global strlen ; export strlen
 
 strlen:
+    enter 0, 0 ; prologue
     mov rcx, -1 ; repne stops if rcx is 0, we set it to -1 so that it never reaches zero
     mov rsi, rdi ; rdi will be modified by scasb so we save it
     mov al, 0 ; scasb checks the current byte against the value stored in al
@@ -11,4 +12,5 @@ strlen:
     sub rdi, rsi ; subtract the start pointer from the current pointer to get the length
     dec rdi ; remove one fron the length (the null terminator)
     mov rax, rdi ; move the result to rax
+    leave ; epilogue
     ret ; return the value
