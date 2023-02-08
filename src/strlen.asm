7128d91ef3c2c5efaaad61cbc1fc58d72fec71d7
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-02-08 22:59:23 +0100

    refactor: format comments in a cleaner way

--- src/strlen.asm
+++ src/strlen.asm
@@ -1,16 +1,16 @@
-bits 64 ; set the register sizes to 64 bits
-
-section .text
-    global strlen ; export strlen
-
-strlen:
-    enter 0, 0 ; prologue
-    mov rcx, -1 ; repne stops if rcx is 0, we set it to -1 so that it never reaches zero
-    mov rsi, rdi ; rdi will be modified by scasb so we save it
-    mov al, 0 ; scasb checks the current byte against the value stored in al
-    repne scasb ; repeat scasb while the zf flag is not set
-    sub rdi, rsi ; subtract the start pointer from the current pointer to get the length
-    dec rdi ; remove one fron the length (the null terminator)
-    mov rax, rdi ; move the result to rax
-    leave ; epilogue
-    ret ; return the value
+bits 64                                 ; set the register sizes to 64 bits
+    
+section .text   
+    global strlen                       ; export strlen
+    
+strlen: 
+    enter 0, 0                          ; prologue
+    mov rcx, -1                         ; repne stops if rcx is 0, we set it to -1 so that it never reaches zero
+    mov rsi, rdi                        ; rdi will be modified by scasb so we save it
+    mov al, 0                           ; scasb checks the current byte against the value stored in al
+    repne scasb                         ; repeat scasb while the zf flag is not set
+    sub rdi, rsi                        ; subtract the start pointer from the current pointer to get the length
+    dec rdi                             ; remove one fron the length (the null terminator)
+    mov rax, rdi                        ; move the result to rax
+    leave                               ; epilogue
+    ret                                 ; return the value
