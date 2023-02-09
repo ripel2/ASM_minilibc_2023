519655edbdf393e171808bc6721540ac431c28da
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-02-09 16:46:17 +0100

    test: add more tests for memset

--- tests/tests_memset.c
+++ tests/tests_memset.c
@@ -132,3 +132,31 @@ Test(my_memset, int_list_empty, .init = init_memset, .timeout = 2)
     cr_assert_eq(list[8], 9);
     cr_assert_eq(list[9], 10);
 }
+
+Test(my_memset, unaligned_set_1, .init = init_memset, .timeout = 2)
+{
+    long long list[] = {0xffffffffffff, 0xffff0000ffff, 0xf0f0f0f0, 0x0f0f0f0f, 0x12345678, 0x87654321};
+
+    my_memset(list, 0xff, 3 * sizeof(long long int));
+    cr_assert_eq(list[0], 0xffffffffffffffff);
+    cr_assert_eq(list[1], 0xffffffffffffffff);
+    cr_assert_eq(list[2], 0xffffffffffffffff);
+    cr_assert_eq(list[3], 0x0f0f0f0f);
+    cr_assert_eq(list[4], 0x12345678);
+    cr_assert_eq(list[5], 0x87654321);
+}
+
+Test(my_memset, unaligned_set_2, .init = init_memset, .timeout = 2)
+{
+    short list[] = {0xffff, 0x0000, 0xf0f0, 0x0f0f, 0x1234, 0x5678, 0x8765, 0x4321};
+
+    my_memset(list, 0xff, 3 * sizeof(short));
+    cr_assert_eq(list[0], 0xffffffff);
+    cr_assert_eq(list[1], 0xffffffff);
+    cr_assert_eq(list[2], 0xffffffff);
+    cr_assert_eq(list[3], 0x0f0f);
+    cr_assert_eq(list[4], 0x1234);
+    cr_assert_eq(list[5], 0x5678);
+    cr_assert_eq(list[6], 0xffff8765);
+    cr_assert_eq(list[7], 0x4321);
+}
