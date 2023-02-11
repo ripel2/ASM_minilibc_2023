6e1ec17b14e26bdd8f774d074ace1c9531395e8a
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-02-11 22:09:56 +0100

    refactor: add comments to memcpy and check for second argument

--- src/memcpy.asm
+++ src/memcpy.asm
@@ -4,16 +4,20 @@ section .text
     global memcpy                       ; export memcpy
 
 memcpy:
-    enter 0, 0
-    cmp rdx, 0
-    je return
-    mov rcx, 0
+    enter 0, 0                          ; prelude
+    cmp rdi, 0                          ; check if first ptr is equal to zero (NULL)
+    je return                           ; if it is, return
+    cmp rsi, 0                          ; check if second ptr is equal to zero (NULL)
+    je return                           ; if it is, return
+    mov rcx, 0                          ; set rcx to 0, used as a counter for the current byte to copy
+  
 set_and_loop:
-    mov r8b, byte [rsi + rcx]
-    mov byte [rdi + rcx], r8b
-    inc rcx
-    cmp rdx, rcx
-    jne set_and_loop
+    mov r8b, byte [rsi + rcx]           ; store the current byte in a temporary byte register
+    mov byte [rdi + rcx], r8b           ; copy the byte in the other address
+    inc rcx                             ; increment the counter
+    cmp rdx, rcx                        ; compare the counter and the size argument
+    jne set_and_loop                    ; if we didnt reach the end we need to continue tne loop
+
 return:
-    leave
-    ret
+    leave                               ; epilogue
+    ret                                 ; return the value
