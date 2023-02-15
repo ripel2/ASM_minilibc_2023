8b68c137fbc9d4ca95f7158eacbe3324e9e7cda9
Lucas Hauszler <lucas.hauszler@epitech.eu>
2023-02-15 22:30:48 +0100

    refactor: add comments to strrchr

--- src/strrchr.asm
+++ src/strrchr.asm
@@ -4,25 +4,25 @@ section .text
     global strrchr                      ; export strrchr
 
 strrchr:
-    movzx eax, byte [rdi]
-    cmp al, 0
-    je set_zero
-    mov edx, 0
+    movzx eax, byte [rdi]               ; get the first byte of the string in a 4 byte register
+    cmp al, 0                           ; check if it is zero
+    je set_zero                         ; if it is, jump to set zero (reached end of string)
+    mov rdx, 0                          ; initialize the result to zero
 
 check_and_loop:
-    cmp esi, eax
-    cmove rdx, rdi
-    inc rdi
-    movzx eax, byte [rdi]
-    cmp al, 0
-    jne check_and_loop
+    cmp esi, eax                        ; compare the current character against c
+    cmove rdx, rdi                      ; set the result to the current address if it is c
+    inc rdi                             ; increment the current char address
+    movzx eax, byte [rdi]               ; get the next byte of the string in a 4 byte register
+    cmp al, 0                           ; check if we reached the end of the string
+    jne check_and_loop                  ; if not, continue the loop
 
 verif_null:
-    cmp esi, 0
-    cmove rdx, rdi
-    mov rax, rdx
-    ret
+    cmp esi, 0                          ; if we need to return the result, we check if c is \0
+    cmove rdx, rdi                      ; if it is we need to return the position of \0 so we set the result to it
+    mov rax, rdx                        ; put the result in the return register
+    ret                                 ; return
 
 set_zero:
-    mov edx, 0
-    jmp verif_null
+    mov rdx, 0                          ; set the result to zero
+    jmp verif_null                      ; jump to check if c is \0
